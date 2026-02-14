a7e06c12b9801782f6d4c357e9f5ff1773ddd496
Mike Chu <git@mikechu.com>
2026-02-14 07:16:48 -0500

    test: remove test comment to verify branch protection

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 // PostureLens - App Bootstrap & Orchestrator
 // Wires together modules: capabilities, UI, canvas, capture, detection
-// Testing branch protection - this should be blocked from direct push
 
 import type { Results } from '@mediapipe/holistic'
 import { AlertEngine } from './core/alert-engine.ts'
